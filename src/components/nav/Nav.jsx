f3f5873143a45456c4e8a82cbc285d246e950ea5
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-27 14:57:07 +0200

    menu burger

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,44 +1,61 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './nav.css'
 import logo from '/src/assets/shared/logo.svg'
+import burgerIcon from '/src/assets/shared/icon-hamburger.svg'
+import closeIcon from '/src/assets/shared/icon-close.svg'
 
 export default function Nav() {
-    return(
-        <>
-            <div className='divNav'>
-                <div className='navDiv1'>
-                    <img className='navImg' src={logo} alt="logo site" />
-                </div>
-                <nav className='navDiv2'>
-                    <NavLink 
-                        className={({ isActive }) => isActive ? 'link active' : 'link'} 
-                        to="/"
-                    >
-                        <p className='navP'>00 HOME</p>
-                    </NavLink>
-                    
-                    <NavLink 
-                        className={({ isActive }) => isActive ? 'link active' : 'link'} 
-                        to="/destination"
-                    >
-                        <p className='navP'>01 DESTINATION</p>
-                    </NavLink>
-                    
-                    <NavLink 
-                        className={({ isActive }) => isActive ? 'link active' : 'link'} 
-                        to="/crew"
-                    >
-                        <p className='navP'>02 CREW</p>
-                    </NavLink>
-                    
-                    <NavLink 
-                        className={({ isActive }) => isActive ? 'link active' : 'link'} 
-                        to="/technology"
-                    >
-                        <p className='navP'>03 TECHNOLOGY</p>
-                    </NavLink>
-                </nav>
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+
+    return (
+        <div className='divNav'>
+            <div className='navDiv1'>
+                <img className='navImg' src={logo} alt="logo site" />
             </div>
-        </>
+
+            
+           <button className="burgerBtn" onClick={toggleMenu}>
+                <img
+                    src={menuOpen ? closeIcon : burgerIcon}
+                    alt={menuOpen ? "Fermer le menu" : "Ouvrir le menu"}
+                    className="burgerIcon"
+                />
+                </button>
+
+            <nav className={`navDiv2 ${menuOpen ? 'open' : ''}`}>
+                <NavLink 
+                    className={({ isActive }) => isActive ? 'link active' : 'link'} 
+                    to="/" onClick={() => setMenuOpen(false)}
+                >
+                    <p className='navP'>00 HOME</p>
+                </NavLink>
+
+                <NavLink 
+                    className={({ isActive }) => isActive ? 'link active' : 'link'} 
+                    to="/destination" onClick={() => setMenuOpen(false)}
+                >
+                    <p className='navP'>01 DESTINATION</p>
+                </NavLink>
+
+                <NavLink 
+                    className={({ isActive }) => isActive ? 'link active' : 'link'} 
+                    to="/crew" onClick={() => setMenuOpen(false)}
+                >
+                    <p className='navP'>02 CREW</p>
+                </NavLink>
+
+                <NavLink 
+                    className={({ isActive }) => isActive ? 'link active' : 'link'} 
+                    to="/technology" onClick={() => setMenuOpen(false)}
+                >
+                    <p className='navP'>03 TECHNOLOGY</p>
+                </NavLink>
+            </nav>
+        </div>
     )
 }
