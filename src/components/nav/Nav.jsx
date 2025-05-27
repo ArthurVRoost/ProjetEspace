61fb9005a32b54cfaa8ae8a1be38fd433e8cefd9
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-27 10:52:03 +0200

    link nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './nav.css'
 import logo from '/src/assets/shared/logo.svg'
 export default function Nav() {
@@ -8,10 +9,14 @@ export default function Nav() {
                     <img className='navImg' src={logo} alt="logo site" />
                 </div>
                 <div className='navDiv2'>
-                    <p className='navP'>00 HOME</p>
-                    <p className='navP'>01 DESTINATION</p>
-                    <p className='navP'>02 CREW</p>
-                    <p className='navP'>03 TECHNOLOGY</p>
+                    <Link to="/"><p className='navP'>00 HOME</p></Link>
+                    
+                    <Link to="/destination"> <p className='navP'>01 DESTINATION</p></Link>
+                    <Link to="/crew"><p className='navP'>02 CREW</p></Link>
+                    <Link to="/technology"><p className='navP'>03 TECHNOLOGY</p></Link>
+                   
+                    
+                    
                 </div>
             </div>
         </>
