91873ca57308efd16e6b2e243ab48e5999f2e91e
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-05-28 14:16:19 +0200

    useNavigate home

--- src/pages/accueil/Accueil.jsx
+++ src/pages/accueil/Accueil.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Nav from '../../components/nav/Nav'
 import './accueil.css'
 import bgHome from '/src/assets/home/background-home-desktop.jpg'
 
 export default function Accueil() {
+    const navigate = useNavigate()
     return(
         <>
             <div className='bodyAccueil' style={{backgroundImage: `url(${bgHome})`, width:"100vw", backgroundSize: 'cover', height: "100vh"}}>
@@ -14,8 +15,8 @@ export default function Accueil() {
                         <h1 className='accueilH1'>SPACE</h1>
                         <p className='accueilP'>Let's face it: if you want to go to spac, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relac becayse we'll give you a truly out of this world experience!</p>
                     </div>
-                    <div className='accueilDiv2 pulse'>
-                        <Link className='link' to="/destination"><h2 className='accueilH22'>EXPLORE</h2></Link>
+                    <div className='accueilDiv2 pulse' onClick={()=>navigate("/destination")}>
+                        <h2 className='accueilH22'>EXPLORE</h2>
                         
                     </div>
                  </div>
